0788224968ae3294844f31176e381bd16d964714
Ralah Aaqil <ralah@riffle.studio>
2026-02-13 14:34:30 -0800

    Add map

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
+from ipyleaflet import Map
 from shiny import App, ui
+from shinywidgets import output_widget, render_widget
 
 app_ui = ui.page_sidebar(
     ui.sidebar("Tabs/Views", bg="#f8f8f8"),
@@ -16,7 +18,7 @@ app_ui = ui.page_sidebar(
             col_widths=(3, 3, 6),
         ),
         ui.layout_columns(
-            ui.card("Map"),
+            ui.card(output_widget("map")),
             ui.card("Filters"),
             col_widths=(10, 2),
         ),
@@ -28,7 +30,14 @@ app_ui = ui.page_sidebar(
 
 
 def server(input, output, session):
-    pass
+    @render_widget
+    def map():
+        return Map(
+            center=(49.25, -123.12),
+            zoom=12.2,
+            min_zoom=12.2,
+            scroll_wheel_zoom=True,
+        )
 
 
 app = App(app_ui, server=server)
