e6c6ec4720d1a58e0b56a4d21704e1e0a286518e
Ralah Aaqil <ralah@riffle.studio>
2026-02-13 13:45:09 -0800

    Basic app layout

---
+++ src/app.py
@@ -0,0 +1,32 @@
+from shiny import App, ui
+
+app_ui = ui.page_fillable(
+    ui.layout_columns(
+        ui.layout_columns(
+            ui.card("Chart/Metrics"),
+            ui.card("Chart/Metrics"),
+            ui.layout_columns(
+                ui.card("Chart/Metrics"),
+                ui.card("Chart/Metrics"),
+                ui.card("Chart/Metrics"),
+                col_widths=(6, 6, 12),
+                row_heights=(1, 1),
+            ),
+            col_widths=(3, 3, 6),
+        ),
+        ui.layout_columns(
+            ui.card("Map"),
+            ui.card("Filters"),
+            col_widths=(10, 2),
+        ),
+        col_widths=(12, 12),
+        row_heights=(1, 3),
+    ),
+)
+
+
+def server(input, output, session):
+    pass
+
+
+app = App(app_ui, server=server)
