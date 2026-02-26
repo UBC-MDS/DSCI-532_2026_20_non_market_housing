9e18255ba22b6876ef69cafcdf94a23e53fb4530
Ralah Aaqil <ralah@riffle.studio>
2026-02-26 15:10:15 -0800

    Updated app layout

--- src/app.py
+++ src/app.py
@@ -3,27 +3,25 @@ from shiny import App, ui
 from shinywidgets import output_widget, render_widget
 
 app_ui = ui.page_sidebar(
-    ui.sidebar("Tabs/Views", bg="#f8f8f8"),
+    ui.sidebar("Filters", bg="#f8f8f8"),
     ui.layout_columns(
         ui.layout_columns(
-            ui.card("Chart/Metrics"),
-            ui.card("Chart/Metrics"),
             ui.layout_columns(
-                ui.card("Chart/Metrics"),
-                ui.card("Chart/Metrics"),
-                ui.card("Chart/Metrics"),
-                col_widths=(6, 6, 12),
-                row_heights=(1, 1),
+                ui.card("Total count"),
+                ui.card("Clientele Bar Chart"),
+                col_widths=(12, 12),
+                row_heights=(1, 2),
             ),
-            col_widths=(3, 3, 6),
+            ui.card("Occupancy Year Line Chart"),
+            ui.card("Design Pie Chart"),
+            col_widths=(4, 5, 3),
         ),
         ui.layout_columns(
             ui.card(output_widget("map")),
-            ui.card("Filters"),
-            col_widths=(10, 2),
+            col_widths=(12,),
         ),
         col_widths=(12, 12),
-        row_heights=(1, 3),
+        row_heights=(2, 3),
     ),
     fillable=True,
 )
