dcd3fea60e044ebcc26190e7f8687e2c8ca52b92
jyjzhang <zhang.y.julia@gmail.com>
2026-02-27 11:13:07 -0800

    Year Filter (#51)
    * load data in app
    
    * add reactive calc frame and year filter
    
    * adjust @reactive calc function so query runs
    
    * adjust @reactive.calc definition to be consistent with app
    
    * change @reactive.calc name to be conistent with documentation

--- src/app.py
+++ src/app.py
@@ -1,9 +1,24 @@
 from ipyleaflet import Map
-from shiny import App, ui
+from shiny import App, ui, reactive, render
 from shinywidgets import output_widget, render_widget
+import pandas as pd
+
+clean_df = pd.read_csv(
+    "data/processed/clean-non-market-housing.csv",
+    dtype={"Occupancy Year": "Int64"}
+    )
 
 app_ui = ui.page_sidebar(
-    ui.sidebar("Filters", bg="#f8f8f8"),
+    ui.sidebar(
+        ui.input_slider(
+                id="input_year",
+                label="Occupancy Year",
+                min=clean_df["Occupancy Year"].min(),
+                max=clean_df["Occupancy Year"].max(),
+                value=[clean_df["Occupancy Year"].min(), clean_df["Occupancy Year"].max()],
+                sep=""
+            ),
+        bg="#f8f8f8"),
     ui.layout_columns(
         ui.layout_columns(
             ui.layout_columns(
@@ -28,6 +43,22 @@ app_ui = ui.page_sidebar(
 
 
 def server(input, output, session):
+
+    @reactive.calc
+    def filtered_df():
+        local_area = input.input_local_area()
+        operator = input.input_operator()
+        year_min, year_max = input.input_year()
+        status = input.input_status()
+
+        return clean_df.query(
+            "`Local Area` in @local_area & "
+            "`Operator` in @operator & "
+            "`Occupancy Year` >= @year_min & "
+            "`Occupancy Year` <= @year_max & "
+            "`Project Status` in @status"
+        )
+
     @render_widget
     def map():
         return Map(
