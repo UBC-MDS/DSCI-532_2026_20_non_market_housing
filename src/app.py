bf0eda43cf6d9456b8c5ce4bae180989d51f610c
Yuting Ji <yutingj894@gmail.com>
2026-02-28 12:16:11 +0800

    switch to absolute import for local test

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ import pandas as pd
 from shapely import wkt
 from ipywidgets import HTML
 
-from .charts.accessibility_pie import create_accessibility_pie_chart
-from .charts.clientele_bar_chart import make_clientele_bar_chart
-from .charts.occupancy_line_chart import make_occupancy_line_chart
+from charts.accessibility_pie import create_accessibility_pie_chart
+from charts.clientele_bar_chart import make_clientele_bar_chart
+from charts.occupancy_line_chart import make_occupancy_line_chart
 
 clean_df = pd.read_csv(
     "data/processed/clean-non-market-housing.csv",
