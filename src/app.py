c4619bd6c3d9521094e3173ce5619533c682d722
Ralah Aaqil <94609279+ralahaaqil@users.noreply.github.com>
2026-02-27 18:29:08 -0800

    Fix relative import issue (#63)

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from shapely import wkt
 from ipywidgets import HTML
 
-from charts.accessibility_pie import create_accessibility_pie_chart
+from .charts.accessibility_pie import create_accessibility_pie_chart
 
 clean_df = pd.read_csv(
     "data/processed/clean-non-market-housing.csv",
