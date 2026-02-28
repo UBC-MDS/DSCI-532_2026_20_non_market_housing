1d9a636cd6ec97ee65fdc10c7c1500571226d7b4
Yuting Ji <yutingj894@gmail.com>
2026-02-28 12:22:02 +0800

    change to relative import

--- src/app.py
+++ src/app.py
@@ -6,8 +6,8 @@ from shapely import wkt
 from ipywidgets import HTML
 
 from .charts.accessibility_pie import create_accessibility_pie_chart
-from charts.clientele_bar_chart import make_clientele_bar_chart
-from charts.occupancy_line_chart import make_occupancy_line_chart
+from .charts.clientele_bar_chart import make_clientele_bar_chart
+from .charts.occupancy_line_chart import make_occupancy_line_chart
 
 clean_df = pd.read_csv(
     "data/processed/clean-non-market-housing.csv",
