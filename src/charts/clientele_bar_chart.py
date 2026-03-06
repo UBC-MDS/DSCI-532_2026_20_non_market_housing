1982ec49c4d9f241ac2739b985da681f0c926710
j2322zha <zhang.y.julia@gmail.com>
2026-03-05 21:55:47 -0800

    add bar chart tooltips

--- src/charts/clientele_bar_chart.py
+++ src/charts/clientele_bar_chart.py
@@ -45,6 +45,10 @@ def make_clientele_bar_chart(d: pd.DataFrame) -> alt.Chart:
             x=alt.X("Clientele:N", title=None),
             y=alt.Y("Units:Q", title="Total units"),
             color=alt.Color("Clientele:N", scale=alt.Scale(scheme="viridis"), legend=None),
+            tooltip=[
+                alt.Tooltip("Clientele:N", title="Clientele"),
+                alt.Tooltip("Units:Q", title="Total number of units", format=",")
+            ],
         )
         .properties(title="Clientele unit totals", height="container", width="container")
         .configure_view(stroke=None)
